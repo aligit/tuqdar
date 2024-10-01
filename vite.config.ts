6c807b3771c2d40f464e1bf345104b12f0f72cf5
alifakheri <alifakheri@protonmail.com>
2024-10-01 04:19:26 +0330

    adds prerenderer

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest" />
 
 import { defineConfig } from 'vite';
-import analog from '@analogjs/platform';
+import analog, { type PrerenderContentFile } from '@analogjs/platform';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -19,6 +19,35 @@ export default defineConfig(({ mode }) => ({
           supportAnalogFormat: true,
         },
         inlineStylesExtension: 'scss',
+      },
+      prerender: {
+        routes: async () => [
+          '/',
+          '/about',
+          '/contact',
+          {
+            contentDir: 'src/content/properties',
+            transform: (file: PrerenderContentFile) => {
+              // Access 'draft' using bracket notation
+              if (file.attributes['draft']) {
+                return false;
+              }
+              // Use the slug from attributes if defined, otherwise use the file's basename
+              const slug = file.attributes['slug'] || file.name;
+              return `/property/${slug}`;
+            },
+          },
+        ],
+        sitemap: {
+          host: 'https://www.yourdomain.com/',
+        },
+        postRenderingHooks: [
+          async (route) => {
+            // Add custom analytics script or other post-rendering modifications
+            const analyticsScript = `<script>/* Your analytics code */</script>`;
+            route.contents = route.contents?.concat(analyticsScript);
+          },
+        ],
       },
     }),
   ],
