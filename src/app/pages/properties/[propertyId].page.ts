cb91e5911cc4ac295dce95c385f49e5c548a3094
alifakheri <alifakheri@protonmail.com>
2024-10-18 04:48:18 +0330

    tempo commit that used to works with hyperlinks within .analog page tempplate

--- src/app/pages/properties/[propertyId].page.ts
+++ src/app/pages/properties/[propertyId].page.ts
@@ -1,18 +1,18 @@
 import { AsyncPipe, NgIf } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { MarkdownComponent, injectContent } from '@analogjs/content';
 import { PropertyAttributes } from './models';
 
 @Component({
   standalone: true,
-  imports: [AsyncPipe, NgIf, MarkdownComponent],
+  imports: [AsyncPipe, MarkdownComponent],
   template: `
-  <ng-container *ngIf="property$ | async as property">
-
-    <h1>{{ property.attributes.title }}</h1>
-    <analog-markdown [content]="property.content"></analog-markdown>
-    </ng-container>
-
+    @if (property$ | async; as property) {
+      <h1>{{ property.attributes.title }}</h1>
+      <analog-markdown [content]="property.content"></analog-markdown>
+    } @else {
+      <p>Loading property details...</p>
+    }
   `,
 })
 export default class PropertyComponent {
