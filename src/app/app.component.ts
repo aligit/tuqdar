82549611d98d960186ddc5e897293130153f976a
alifakheri <alifakheri@protonmail.com>
2024-11-06 08:19:00 +0330

    Ok now this works very well

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,31 +13,31 @@ import { IconSvgService } from './services/icon-svg.service';
       <router-outlet></router-outlet>
     </div>
   `,
-  styles: [`
-    :host {
-      display: flex;
-      flex-direction: column;
-      min-height: 100vh;
-      width: 100%;
-      margin: 0;
-      padding: 0;
-    }
-    .content {
-      flex: 1;
-      padding-top: 64px;
-      padding-left: 2rem;
-      padding-right: 2rem;
-      max-width: 1280px;
-      margin: 0 auto;
-      width: 100%;
-      box-sizing: border-box;
-    }
-    @media (max-width: 600px) {
-      .content {
-        padding-top: 56px;
+  styles: [
+    `
+      :host {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+        width: 100%;
+        margin: 0;
+        padding: 0;
       }
-    }
-  `],
+      .content {
+        flex: 1;
+        padding-top: 64px;
+        max-width: 100%;
+        margin: 0 auto;
+        width: 100%;
+        box-sizing: border-box;
+      }
+      @media (max-width: 600px) {
+        .content {
+          padding-top: 56px;
+        }
+      }
+    `,
+  ],
 })
 export class AppComponent {
   private iconSvgService = inject(IconSvgService);
