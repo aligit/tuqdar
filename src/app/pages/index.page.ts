d1391173e7e37e563817e55042b3d055864323c1
alifakheri <alifakheri@protonmail.com>
2025-01-31 17:55:29 +0330

    replace vila w/ apartment

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -16,11 +16,10 @@ import { FeaturedPropertiesComponent } from '../components/featured-properties/f
     <app-banner
       imageSrc="/images/index-banner.jpeg"
       imageAlt="Toghdar Banner"
-      title="ویلاهای ساحلی فرح آباد"
     ></app-banner>
 
     <app-featured-properties
-      [title]="'ویلاهای ویژه'"
+      [title]="'املاک دینجی'"
       [properties]="properties"
     ></app-featured-properties>
   `,
@@ -34,22 +33,22 @@ export default class HomeComponent {
   //Make sure to replace images with better alternatives
   properties = [
     {
-      image: '/images/3.jpg',
+      image: 'https://s100.divarcdn.com/static/photo/neda/post/d_D8umgFAaf-oTTLBY_IJg/43719b03-b03d-4291-b1b9-730855eb98ee.jpg',
       title: 'ویلا دوبلکس مدرن(  شمالی )',
       description: '۵۰۰ متر تا دریا، سکوت و آرامش',
     },
     {
-      image: '/images/1.jpeg',
+      image: 'https://s100.divarcdn.com/static/photo/neda/post/mZv2sRx2LdlaOQKKe0fePQ/7b7072e4-4fca-460a-a614-27f891d40107.jpg',
       title: 'ویلا دوبلکس مدرن( جنوبی )',
       description: '۳ اتاق خواب، استخر و پارکینگ',
     },
     {
-      image: '/images/pamchal/3.JPEG',
+      image: 'https://s100.divarcdn.com/static/photo/neda/post/26l6ijMSew41AQjuwP1acw/95553005-8a31-49a2-8b26-4c9dbfe683cf.jpg',
       title: 'فلت مدرن  ۱۶۰ متری',
       description: 'دنج و با صفا و آرام ده دقیقه تا ساحل، فرنیش کامل',
     },
     {
-      image: '/images/shirinbol/4.jpg',
+      image: 'https://s100.divarcdn.com/static/photo/neda/post/MM7MEJh-jPR_Mazcb2KhaA/db2a6890-12d4-4f53-bd88-dbeda66327af.jpg',
       title: 'ویلا باغ لوکس',
       description: 'شیک، دنج، سرسبز، خلوت و آرام',
     },
